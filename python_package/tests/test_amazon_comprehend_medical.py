5d51f94e66bcb6fbc7264d97e00ef31160433906
Marcos Foglino <marcos.foglino@caylent.com>
2025-06-13 12:13:09 -0300

    improving comprehend test

--- python_package/tests/test_amazon_comprehend_medical.py
+++ python_package/tests/test_amazon_comprehend_medical.py
@@ -5,8 +5,6 @@ import pytest
 comprehend_medical_client = boto3.client("comprehendmedical", region_name="us-east-1")
 
 def test_case_data_with_comprehend_medical(glue_context):
-    # Mock input data from the case_data table
-    case_data_sample = "Patient has a history of diabetes and hypertension."
 
     case_df = read_case_data(glue_context)
     # select field case_interpretation where case_hub_id = 11335065
@@ -22,9 +20,12 @@ def test_case_data_with_comprehend_medical(glue_context):
     response = comprehend_medical_client.detect_entities_v2(Text=case_data_sample)
     print("Response from Comprehend Medical:", response)
 
+    entities = response["Entities"]
+    print("Detected entities:", entities["Text"])
+
     # # Validate the response
     # assert "Entities" in response
-    # entities = response["Entities"]
+
     #
     # # Check for specific medical conditions
     # conditions = [entity["Text"] for entity in entities if entity["Category"] == "MEDICAL_CONDITION"]
@@ -54,3 +55,18 @@ def read_case_data(glue_context):
         .load(input_csv_path)
     )
     return df
+
+def test_comprehend_for_a_paticular_order():
+    # Test for
+    # 6311256
+    # 11132310
+    # 101194376
+    case_data_sample = """
+    6347959,11192339,101802889,,,,,1226X,H&E,,468363,0,4760194,1349,ORDERED,FISH,Histology,IHC,Test,,,RESULTED,,,,,,,,,,,,,,2024-09-30 15:48:03.273,2024-10-03 14:19:10.963,true,2025-06-09,6347959,,BCELL^DLBCL^HEME^NHL,,SOLID TUMOR,,New Diagnosis,71,2025-06-09,11192339,Global,Complete,FISH Global,FSG,FISH,Retroperitoneal Mass,RETROPERITONEUM,Paraffin Tissue,Paraffin Tissue,Unstained Slide(s),IHGL,High-Grade/Large B-Cell Lymphoma FISH,Abnormal,BCL2 rearrangements are observed in double/triple-hit lymphomas, 90% of follicular lymphomas, 30% of diffuse large cell lymphomas (sometimes with prior follicular type), and rarely in other lymphoproliferative disorders.
+    The >2G signal pattern with the MYC/IGH probe set is suggestive of gain of the IGH locus, trisomy 14 or an IGH rearrangement with a gene other than MYC.
+    There is no evidence of MYC or BCL6 rearrangement in this case. Therefore, while this study is ABNORMAL, there is NO EVIDENCE OF A DOUBLE/TRIPLE-HIT LYMPHOMA”.,,2025-06-09,4760194,F,1953-02-09,72,IL,60638,2025-06-09,1349,7247,Loyola University Medical Center Anatomic Patholog,Illinois,IL,60153,United States,US,Academic Medical Center,,Academic Setting,2025-06-09,468363,1326507666,Allopathic & Osteopathic Physicians,Pathology,Anatomic Pathology & Clinical Pathology,PATHOLOGY,IL,606112908,UNITED STATES,US,2025-06-09,0,,,,,OTHER,,,,,2025-06-09,6347959,11192339,101802889,1226X,H&E,101939388,\av1-isilon.neogen.local\clinical\ihc\aperio\images\2024\oct\01\101939388.svs,\av1-isilon.neogen.local|clinical\ihc\aperio\images\2024\oct\01\,101939388.svs,359219521,2024-10-01 02:35:27.447,2024-10-01 09:35:27.447,AV,0,0,First,Image Ready,ss12117,pass,Automatic Instrument QC,Image Ready,2,,,AperioGT450,Aperio Leica Biosystems GT450 v1.0.1,40.00,47963,32456,0.2630000000,,2025-06-09,1349,7247,Loyola University Medical Center Anatomic  
+    """
+    #print("Sample case data for entity detection:", case_data_sample)
+
+    response = comprehend_medical_client.detect_entities_v2(Text=case_data_sample)
+    print("Response from Comprehend Medical:", response)
