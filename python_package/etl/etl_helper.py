908130c65e482d7bd6920b1140215f5adb7315e5
Marcos Foglino <marcos.foglino@caylent.com>
2025-06-26 17:47:48 -0300

    cleaning

--- python_package/etl/etl_helper.py
+++ python_package/etl/etl_helper.py
@@ -253,9 +253,6 @@ def parse_fhir_medication_all_exploded(df):
         extract_concept_class(col("coding_item.extension")).alias("class")
     )
 
-    # Filter out when normalized_concept_standard is null
-    #coding_result_df = coding_result_df.filter(col("standard").isNotNull())
-
     # Part 2: Explode the ingredient array
     exploded_ingredient_df = df.select(
         "id",
---
+++ terraform/step_function.tf
@@ -0,0 +1,118 @@
+resource "aws_sfn_state_machine" "etl_orchestration" {
+  name     = "etl-orchestration"
+  role_arn = aws_iam_role.step_function_role.arn
+
+  definition = jsonencode({
+    Comment = "ETL Pipeline Orchestration"
+    StartAt = "ProcessLandingToRaw"
+    States = {
+      "ProcessLandingToRaw" = {
+        Type = "Parallel"
+        Next = "ProcessRawToStage"
+        Branches = [
+          {
+            StartAt = "ProcessCSVLandingToRaw"
+            States = {
+              "ProcessCSVLandingToRaw" = {
+                Type       = "Task"
+                Resource   = "arn:aws:states:::glue:startJobRun"
+                Parameters = {
+                  JobName = "caylent-poc-etl-landing-to-raw-csv"
+                  Arguments = {
+                    "--landing_bucket_name"    = "neogenomics-caylent-shared-data-daas"
+                    "--datalake_bucket_name"   = "caylent-poc-datalake"
+                    "--raw_namespace"          = "raw"
+                  }
+                }
+                End = true
+              }
+            }
+          },
+          {
+            StartAt = "ProcessFHIRLandingToRaw"
+            States = {
+              "ProcessFHIRLandingToRaw" = {
+                Type       = "Task"
+                Resource   = "arn:aws:states:::glue:startJobRun"
+                Parameters = {
+                  JobName = "caylent-poc-etl-landing-to-raw-fhir"
+                }
+                End = true
+              }
+            }
+          }
+        ]
+      },
+      "ProcessRawToStage" = {
+        Type = "Parallel"
+        End  = true
+        Branches = [
+          {
+            StartAt = "ProcessCSVRawToStage"
+            States = {
+              "ProcessCSVRawToStage" = {
+                Type       = "Task"
+                Resource   = "arn:aws:states:::glue:startJobRun"
+                Parameters = {
+                  JobName = "caylent-poc-etl-raw-to-stage-csv"
+                }
+                End = true
+              }
+            }
+          },
+          {
+            StartAt = "ProcessFHIRRawToStage"
+            States = {
+              "ProcessFHIRRawToStage" = {
+                Type       = "Task"
+                Resource   = "arn:aws:states:::glue:startJobRun"
+                Parameters = {
+                  JobName = "caylent-poc-etl-raw-to-stage-fhir"
+                }
+                End = true
+              }
+            }
+          }
+        ]
+      }
+    }
+  })
+}
+
+resource "aws_iam_role" "step_function_role" {
+  name = "step_function_etl_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "step_function_policy" {
+  name = "step_function_etl_policy"
+  role = aws_iam_role.step_function_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:StartJobRun",
+          "glue:GetJobRun",
+          "glue:GetJobRuns",
+          "glue:BatchStopJobRun"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
