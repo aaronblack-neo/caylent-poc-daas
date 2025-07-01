ea4cbd57993fa7fcc780e270376dbefdd1dd8f1d
RaulPontelloCaylent <raul.pontello@caylent.com>
2025-07-01 12:41:40 -0300

    Adding deployment of comprehend glue job

--- terraform/glue_job.tf
+++ terraform/glue_job.tf
@@ -3,6 +3,7 @@ locals {
   fhir_job_name       = "job_landing_to_raw_fhir.py"
   fhir_stage_job_name = "job_raw_to_stage_fhir.py"
   csv_stage_job_name  = "job_raw_to_stage_csv.py"
+  comprehend_job_name = "job_comprehend.py"
 
   iceberg_spark_conf = <<EOT
  conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions
@@ -158,6 +159,48 @@ resource "aws_glue_job" "csv_stage_job" {
 
   command {
     script_location = "s3://${aws_s3_bucket.glue_scripts_bucket.id}/${aws_s3_object.csv_stage_job_script.key}"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--job-language"                     = "python"
+    "--job-bookmark-option"              = "job-bookmark-disable"
+    "--enable-glue-datacatalog"          = "true"
+    "--enable-metrics"                   = "true"
+    "--enable-job-insights"              = "true"
+    "--enable-observability-metrics"     = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-spark-ui"                  = "true"
+    "--extra-py-files"                   = "s3://${aws_s3_bucket.glue_scripts_bucket.id}/artifacts/python_libs-0.1.0-py3-none-any.whl"
+    "--conf"                             = trim(local.iceberg_spark_conf, "\n")
+    "--extra-jars"                       = "s3://${aws_s3_bucket.glue_scripts_bucket.id}/s3_tables_jars/s3-tables-catalog-for-iceberg-runtime-0.1.5.jar"
+    "--namespace"                        = "stage"
+  }
+
+  glue_version      = "5.0"
+  worker_type       = "G.1X"
+  number_of_workers = "2"
+
+  execution_property {
+    max_concurrent_runs = 10
+  }
+}
+
+# Glue Job for Comprehend
+
+resource "aws_s3_object" "comprehend_job_script" {
+  bucket = aws_s3_bucket.glue_scripts_bucket.id
+  key    = "glue_jobs/${local.comprehend_job_name}"
+  source = "../python_package/glue_jobs/${local.comprehend_job_name}"
+  etag   = filemd5("../python_package/glue_jobs/${local.comprehend_job_name}")
+}
+
+resource "aws_glue_job" "comprehend_job" {
+  name     = "caylent-poc-etl-comprehend"
+  role_arn = aws_iam_role.glue_etl_role.arn
+
+  command {
+    script_location = "s3://${aws_s3_bucket.glue_scripts_bucket.id}/${aws_s3_object.comprehend_job_script.key}"
     python_version  = "3"
   }
 
